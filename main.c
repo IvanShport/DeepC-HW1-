6cec72b4d08f72e3221e4c49ccabe90f05d68dc7
IvanShport <ivan.shport.98@mail.ru>
2018-10-06 15:39:54 +0300

    fix input_row()

--- main.c
+++ main.c
@@ -3,57 +3,58 @@
 #include <string.h>
 #include <stddef.h>
 #define START_SIZE_OF_ROW 32
-#define START_SIZE_OF_TEXT 16
+#define START_SIZE_OF_TEXT 2
 
-char* input_row(char* row_pointer, size_t size);
-size_t choice_rows(char** text, size_t size_text, char** fin_text);
+char* input_row(char* row_pointer);
+size_t choice_rows(char** text, const size_t size_of_text, char*** fin_text);
 char** realloc_for_text(char** text, size_t* size_of_text, const size_t size_of_row);
 char** free_not_used_mem(char** text, size_t* size_of_text, const size_t new_size);
 char** create_text(size_t size_of_text, size_t size_of_row);
+char** find_valid_rows(char** text, const size_t size_of_text, size_t *size_of_fin_text);
 char** input_text(char** text, const size_t size_of_row, size_t* size_of_text);
+char** init_start_text(size_t* size_of_text);
 void output_result(char** result_text, const size_t size_of_result);
 void free_text(char** text, size_t size);
 
 int main() {
-    size_t size_of_row = START_SIZE_OF_ROW;
     size_t size_of_text = START_SIZE_OF_TEXT;
+    size_t size_of_fin_text = START_SIZE_OF_TEXT;
 
-    char** text = create_text(size_of_text, size_of_row);
+    char** text = init_start_text(&size_of_text);
     if (text == NULL) {
         printf("[error]");
         return 0;
     }
 
-    char** temp_text = input_text(text, size_of_row, &size_of_text);
-    if (temp_text == NULL) {
-        printf("[error]");
-        free_text(text, size_of_text);
-        return 0;
-    }
-
-    text = temp_text;
-
-    char** fin_text = create_text(size_of_text, 0);
+    char** fin_text = find_valid_rows(text, size_of_text, &size_of_fin_text);
     if (fin_text == NULL) {
         printf("[error]");
         free_text(text, size_of_text);
         return 0;
     }
 
-    size_t size_of_fin_text = choice_rows(text, size_of_text, fin_text);
+    output_result(fin_text, size_of_fin_text);
 
-    if (size_of_fin_text == size_of_text) {
-        free_text(text, size_of_text);
-        free(fin_text);
+    free(fin_text);
+    free_text(text, size_of_text);
+
+    return 0;
+}
+
+char** init_start_text(size_t* size_of_text) {
+    size_t size_of_row = START_SIZE_OF_ROW;
+
+    char** text = create_text(*size_of_text, size_of_row);
+    if (text == NULL)
+        return NULL;
+
+    char** temp_text = input_text(text, size_of_row, size_of_text);
+    if (temp_text == NULL) {
+        free_text(text, *size_of_text);
         return 0;
     }
 
-    output_result(fin_text, size_of_fin_text);
-
-    free_text(text, size_of_text);
-    free_text(fin_text, size_of_fin_text);
-
-    return 0;
+    return temp_text;
 }
 
 char** create_text(size_t size_of_text, size_t size_of_row) {
@@ -61,7 +62,9 @@ char** create_text(size_t size_of_text, size_t size_of_row) {
     if (text == NULL) {
         return NULL;
     }
-    if (size_of_row == 0) return text;
+
+    if (size_of_row == 0)
+        return text;
 
     for (size_t i = 0; i < size_of_text; i++) {
         text[i] = (char*)malloc(sizeof(char) * size_of_row);
@@ -78,7 +81,7 @@ char** input_text(char** text, const size_t size_of_row, size_t* size_of_text) {
     size_t i = 0;
     for (;; i++) {
 
-        char* temp_row = input_row(text[i], size_of_row);
+        char* temp_row = input_row(text[i]);
         if (temp_row == NULL)
             return NULL;
 
@@ -105,29 +108,39 @@ char** input_text(char** text, const size_t size_of_row, size_t* size_of_text) {
     return text;
 }
 
-char* input_row(char* row_pointer, size_t size) {
-    ptrdiff_t cur_size = 1;
-    size_t empty_size = size;
+char* input_row(char* row_pointer) {
+    ptrdiff_t cur_size = 0;
+    size_t empty_size = 64;
+    size_t filled_size = 0;
+    size_t row_size = 1024;
 
-    char* end_row = row_pointer;
+    char end_row[empty_size];
+    char* temp_row = (char*)realloc(row_pointer, row_size * sizeof(char));
+    if (temp_row == NULL) return NULL;
+    row_pointer = temp_row;
 
     row_pointer[0] = '\0';
     while (fgets(end_row, empty_size, stdin)) {
         if (end_row[strlen(end_row) - 1] == '\n') {
             end_row[strlen(end_row) - 1] = '\0';
+            memcpy(row_pointer + filled_size - cur_size, end_row, strlen(end_row) + 1);
             break;
         }
 
-        empty_size = size;
-        size *= 2;
-        char* temp_row = (char*)realloc(row_pointer, size * sizeof(char));
-        if (temp_row == NULL) return NULL;
-        row_pointer = temp_row;
-        end_row = row_pointer + (empty_size - cur_size);
+        if (filled_size >= row_size) {
+            row_size *= 2;
+            temp_row = (char*)realloc(row_pointer, row_size * sizeof(char));
+            if (temp_row == NULL)
+                return NULL;
+            row_pointer = temp_row;
+        }
+
+        memcpy(row_pointer + filled_size - cur_size, end_row, empty_size);
+        filled_size += empty_size;
         cur_size++;
     }
 
-    char* temp_row = (char*)realloc(row_pointer, (strlen(row_pointer) + 1) * sizeof(char));
+    temp_row = (char*)realloc(row_pointer, (strlen(row_pointer) + 1) * sizeof(char));
     if (temp_row == NULL) {
         free(row_pointer);
         return NULL;
@@ -145,8 +158,12 @@ char** realloc_for_text(char** text, size_t* size_of_text, const size_t size_of_
         return NULL;
     text = temp_text;
 
+    if (size_of_row == 0)
+        return text;
+
     for (size_t i = *size_of_text / 2; i < *size_of_text; i++) {
         text[i] = (char*)malloc(sizeof(char) * size_of_row);
+
         if (text[i] == NULL) {
             *size_of_text = i;
             return NULL;
@@ -170,9 +187,24 @@ char** free_not_used_mem(char** text, size_t* size_of_text, const size_t new_siz
     return text;
 }
 
-size_t choice_rows(char** text, size_t size_text, char** fin_text) {
+char** find_valid_rows(char** text, const size_t size_of_text, size_t *size_of_fin_text) {
+    char** fin_text = create_text(*size_of_fin_text, 0);
+    if (fin_text == NULL)
+        return NULL;
+
+    *size_of_fin_text = choice_rows(text, size_of_text, &fin_text);
+    if (*size_of_fin_text == size_of_text) {
+        free(fin_text);
+        return NULL;
+    }
+
+    return fin_text;
+}
+
+size_t choice_rows(char** text, const size_t size_of_text, char*** fin_text) {
     size_t counter_of_bracket = 0;
     size_t counter_of_row = 0;
+    size_t start_size_of_fin_text = START_SIZE_OF_TEXT;
 
     for (size_t i = 0; *text[i] != '\0'; i++) {
         for (size_t j = 0; j < strlen(text[i]); j++) {
@@ -181,22 +213,19 @@ size_t choice_rows(char** text, size_t size_text, char** fin_text) {
         }
 
         if (counter_of_bracket == 0) {
-            char* temp_row = (char*)malloc(sizeof(char) * (strlen(text[i]) + 1));
-            if (temp_row == NULL) {
-                for (size_t k = 0; k < counter_of_row; k++) {
-                    free(fin_text[k]);
-                }
-                return size_text;
-            }
-            fin_text[counter_of_row] = temp_row;
+            *(*fin_text + counter_of_row) = text[i];
 
-            memcpy(fin_text[counter_of_row], text[i], strlen(text[i]) + 1);
+            if (counter_of_row >= start_size_of_fin_text - 1) {
+                char** temp_text = realloc_for_text(*fin_text, &start_size_of_fin_text, 0);
+                if (temp_text == NULL)
+                    return size_of_text;
+
+                *fin_text = temp_text;
+            }
             counter_of_row++;
         }
-
         counter_of_bracket = 0;
     }
-
     return counter_of_row;
 }
 
